53888e858710d388b995affbecca73244cd8d1a8
Jeffrey Santana <jsantana@watsco.com>
2024-10-01 08:06:21 -0400

    Initial Commit

---
+++ Weather y MAS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather y MAS
+//
+//  Created by Jeffrey Santana on 10/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather y MAS/Weather_y_MASApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_y_MASApp.swift
+//  Weather y MAS
+//
+//  Created by Jeffrey Santana on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_y_MASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
